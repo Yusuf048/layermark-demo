83eb0f2bf490530937c6a0366ba1abe349a6bab6
Yusuf048 <yalpdemir@gmail.com>
2022-09-25 19:01:46 +0300

    Small Import Fixes

--- src/main/java/com/example/layermarkdemo/LayermarkDemoApplication.java
+++ src/main/java/com/example/layermarkdemo/LayermarkDemoApplication.java
@@ -1,16 +1,6 @@
 package com.example.layermarkdemo;
-
-import com.example.layermarkdemo.model.Book;
-import com.example.layermarkdemo.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.sql.DataSource;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class LayermarkDemoApplication{
--- src/main/java/com/example/layermarkdemo/model/Book.java
+++ src/main/java/com/example/layermarkdemo/model/Book.java
@@ -2,7 +2,6 @@ package com.example.layermarkdemo.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
 @Table(name = "books")
