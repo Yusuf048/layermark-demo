ca46d3312bff50ded4ff739155b4891cb56867a3
Yusuf048 <yalpdemir@gmail.com>
2022-09-25 19:10:20 +0300

    Modifications on database
    Deleted data.sql. Changed username and password for login

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO books (author, book_name, img, release_date) VALUES ('Yusuf Alpdemir', 'Book of Yusuf', 'https://static.vecteezy.com/system/resources/previews/002/398/513/original/blank-book-cover-free-vector.jpg', '2012-10-30');
-INSERT INTO books (author, book_name, img, release_date) VALUES ('Michio Kaku', 'Physics of the Future', 'https://productimages.hepsiburada.net/s/27/375/10193285152818.jpg', '2011-11-15');
-INSERT INTO books (author, book_name, img, release_date) VALUES ('Yasin Yalım', 'Sociology', 'https://static.vecteezy.com/system/resources/previews/002/398/513/original/blank-book-cover-free-vector.jpg', '2015-11-15');
-INSERT INTO books (author, book_name, img, release_date) VALUES ('İskender Pala', 'Efsane', 'https://productimages.hepsiburada.net/s/7/500/8798904516658.jpg', '2013-01-01');
-INSERT INTO books (author, book_name, img, release_date) VALUES ('Suzanne Collins', 'The Hunger Games', 'https://images-na.ssl-images-amazon.com/images/I/614SwlZNtJL.jpg', '2008-09-14');
-INSERT INTO books (author, book_name, img, release_date) VALUES ('Suzanne Collins', 'Catching Fire', 'https://upload.wikimedia.org/wikipedia/en/a/a2/Catching_Fire_%28Suzanne_Collins_novel_-_cover_art%29.jpg', '2009-09-01');
-INSERT INTO books (author, book_name, img, release_date) VALUES ('Suzanne Collins', 'Mockingjay', 'https://m.media-amazon.com/images/I/41KNsYsW0xL._AC_SY780_.jpg', '2010-08-24');
